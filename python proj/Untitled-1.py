1c2be253df207375f7e736d4a65b31ece13421d9
tyreekhill000 <nkthebeast75@gmail.com>
2023-02-04 15:50:46 -0500

    java

--- python proj/Untitled-1.py
+++
@@ -1,4 +0,0 @@
-#print("hello")
-Cars = ["Tesla", "Toyota", "Maxda", "Honda"]
-print(Cars[1])
-
---
+++ src/test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class test{
+    public static void main(String[] args) {
+       System.out.println("hello how are you doing"); 
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Hello, what is your name?: ");
+       String name = scanner.nextLine();
+       System.out.println("Hello "+name);
+
+
+    }
+
+
+
+
+}
+
